571ec8eac69995bcc87dc219483519419e91ac73
Asbelll <chrystian_alex@hotmail.com>
2015-10-21 18:47:13 -0200

    Base estrutural do index - Early WIP

--- index.php
+++ index.php
@@ -2,3 +2,49 @@
 require_once "autoload.php";
 require_once "definicoes.php";
 ?>
+<body style="background: url('imagens/1.jpg') fixed; background-size: cover;">
+<div class="ui grey inverted segment" style="height: 99px">
+	<p style="font-size: 40; display: inline; float: left; margin-top: 4px">Ataque Divino</p>
+	<div class="huge ui red secondary pointing menu" style="display: inline-flex; margin-left: 500px; opacity: 0.6; margin-top: 28px;">
+		<a class="active item">
+			Home
+		</a>
+		<a class="item">
+			Placeholder
+		</a>
+		<a class="item">
+			Placeholder
+		</a>
+	</div>
+	<div class="ui inverter segment" style="display: inline; float: right; margin-top: -6px; margin-right: -20px">
+		
+		<div class="mini blue ui vertical labeled icon buttons" style="float: right; margin-top: 2px; margin-right: -10px">
+			<button class="ui button">
+				<i class="sign in icon"></i>
+				Login
+			</button>
+			<button class="ui button">
+				<i class="add user icon"></i>
+				Registrar
+			</button>
+		</div>
+
+		<div class="ui mini input focus">
+			<input type="text" placeholder="Usuário" name="user">
+		</div>
+		<br />
+		<div class="ui mini input focus">
+			<input type="password" placeholder="Senha" name="senha">
+		</div>
+	</div>
+</div>
+
+<div class="ui grey inverted segment" style="width: 1000px; margin: 0 auto; margin-top: 250px; height: 572px">
+	<div class="ui inverted four item menu">
+		<a class="active item">Jogo</a>
+		<a class="item">Placeholder</a>
+		<a class="item">Placeholder</a>
+		<a class="item">Placeholder</a>
+	</div>
+</div>
+</body>
