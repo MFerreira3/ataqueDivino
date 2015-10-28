b429ff88886e7bf402cb96df11174be3962f7a30
Asbelll <chrystian_alex@hotmail.com>
2015-10-27 23:53:14 -0200

    funcoes.php
    Pequenas mudanças na estrutura do código/melhor organização.

--- funcoes.php
+++ funcoes.php
@@ -1,19 +1,25 @@
 <?php
-function mes($numero) {
-$mes = array(
-	1 => 'Janeiro',
-	2 => 'Fevereiro',
-	3 => 'Março',
-	4 => 'Abril',
-	5 => 'Maio',
-	6 => 'Junho',
-	7 => 'Julho',
-	8 => 'Agosto',
-	9 => 'Setembro',
-	10 => 'Outubro',
-	11 => 'Novembro',
-	12 => 'Dezembro'
-);
-return $mes[$numero];
+/**
+ * Recebe um valor de mês e retorna o seu respectivo nome;
+ * @param	integer 	$numeroMes	Variável de valor para o mês;
+ * @return	string 		Retorna o nome do mês;
+ */
+function mesNome($numeroMes) {
+	$mes = array(
+		1 => 'Janeiro',
+		2 => 'Fevereiro',
+		3 => 'Março',
+		4 => 'Abril',
+		5 => 'Maio',
+		6 => 'Junho',
+		7 => 'Julho',
+		8 => 'Agosto',
+		9 => 'Setembro',
+		10 => 'Outubro',
+		11 => 'Novembro',
+		12 => 'Dezembro'
+	);
+
+	return $mes[$numeroMes];
 }
 ?>
