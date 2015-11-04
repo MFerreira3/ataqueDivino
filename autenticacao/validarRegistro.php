3b70d43ff8deeed77e21f76e887e5c066d42feba
Matheus Reis <goodnews.room@hotmail.com>
2015-11-04 00:08:57 -0200

    validarRegistro.php
    Adicionadas novas validações para os dados de usuário, senha, e-mail, data de
    nascimento e verificação reCAPTCHA.

--- autenticacao/validarRegistro.php
+++ autenticacao/validarRegistro.php
@@ -1,58 +1,89 @@
 <?php
-/*
-Dados recebidos:
-usuario
-email
-senha
-diaNascimento
-mesNascimento
-anoNascimento
-captcha
-
-Dados retornados:
-resultado
- */
-
-
-
-/*
-Lista de validação
-Usuario = Ta preenchido? Tem menos q 25 caracteres? Caracteres válidos? STR?
-
-Email = Ta preenchido? Tem menos q 100 caracteres? É e-mail?
-
-Senha = Ta preenchida? min 8 caracteres? Max 32?
-
-Datas de Nascimento = Tão preenchidas? N tem caractere retardado? N é menor q 1? N é maior q o mês permite? Juntas dão mais q 13 anos?
-
-reCAPTCHA = Ta preenchido? Validado pelo Google?
- */
 $def_imprimirHTML = false;
 require_once "../definicoes.php";
 require_once "../restricaoAjax.php";
 
-// Impede que uma pessoa já logada realize a criação de uma nova conta
+const SECRET_KEY = "6LchDBATAAAAALY7KMB15GjZukkd3wYj-oZmboJv"; // Secret key do google reCAPTCHA
+
+// Impede que uma pessoa já logada realize a criação de uma nova conta.
 if (!empty($_SESSION['usuario'])) {
-	retornarResultado("g0");
+	retornarResultado("g0a");
 }
 
 /* Validações para o campo usuario
 * 1. usuario não foi preenchido ou não é string?
-* 2. usuario ultrapassa o limite de caracteres
-* 3. usuario não contém caracteres suficiente
-* 4. usuario não contém caracteres inválidos
+* 2. usuario ultrapassa o limite de caracteres?
+* 3. usuario não contém caracteres suficiente?
+* 4. usuario não contém caracteres inválidos?
 */
 if (empty($_POST['usuario']) || !is_string($_POST['usuario'])) {
-	retornarResultado('c0');
+	retornarResultado('c0a');
 } else if (strlen($_POST['usuario']) > 25) {
-	retornarResultado('c1');
+	retornarResultado('c0b');
 } else if (strlen($_POST['usuario']) < 3) {
-	retornarResultado('c2');
+	retornarResultado('c0c');
 } else if (preg_replace("/[^a-zA-Z0-9\/_-]/", "", $_POST['usuario']) !== $_POST['usuario']) {
-	retornarResultado('c3');
+	retornarResultado('c0d');
 }
 
+/* Validações para o campo email
+* 1. email não foi preenchido, não é string ou não é considerado válido?
+* 2. email ultrapassa o limite de caracteres?
+*/
+if (empty($_POST['email']) || !is_string($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	retornarResultado('c1a');
+} else if (strlen($_POST['email']) > 100) {
+	retornarResultado('c1b');
+}
 
+/* Validações para o campo senha
+* 1. senha não foi preenchida?
+* 2. senha ultrapassa o limite de caracteres?
+* 3. senha não contém caracteres suficiente?
+*/
+if (empty($_POST['senha'])) {
+	retornarResultado('c2a');
+} else if (strlen($_POST['senha']) > 32) {
+	retornarResultado('c2b');
+} else if (strlen($_POST['senha']) < 8) {
+	retornarResultado('c2c');
+}
+
+/* Validações para os campos de data
+* 1. diaNascimento, mesNascimento ou anoNascimento não foram preenchidos?
+* 2. Juntos os valores formam uma data válida?
+* 3. O usuário tem 13 ou mais anos de idade?
+*/
+if (empty($_POST['diaNascimento']) || empty($_POST['mesNascimento']) || empty($_POST['anoNascimento'])) {
+	retornarResultado('c3a');
+} else if (!checkdate($_POST['mesNascimento'], $_POST['diaNascimento'], $_POST['anoNascimento'])) {
+	retornarResultado('c3b');
+} else {
+	$dataServidor = new DateTime(date("Y-m-d")); // Instancia um novo objeto date com a data atual.
+	$dataUsuario = new DateTime($_POST['anoNascimento'] . '-' . $_POST['mesNascimento'] . '-' . $_POST['diaNascimento']); // Faz o mesmo com a data enviada pelo usuário.
+	$intervalo = $dataServidor->diff($dataUsuario); // Obtém a diferença entre as duas datas.
+
+	if ($intervalo->y < 13) {
+		retornarResultado('c3c');
+	}
+}
+
+/* Validações do reCAPTCHA
+* 1. O g-response foi enviado?
+* 2. A verificação foi validada pelo google?
+*/
+if (empty($_POST['g-response'])) {
+	retornarResultado('c4a');
+} else {
+	$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . SECRET_KEY . "&response=" . $_POST['g-response'] . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+	$response = json_decode($response);
+
+	if (!$response->success) {
+		retornarResultado('c4b');
+	}
+}
+
+// Se nenhum dado for considerado inválido, será enviado um código de sucesso.
 retornarResultado(0);
 
 function retornarResultado($resultado = 0) {
