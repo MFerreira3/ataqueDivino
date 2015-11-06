770761e826d5de95dd1aef68dd847dd91d0dcad9
Matheus Reis <goodnews.room@hotmail.com>
2015-11-06 19:36:50 -0200

    email.php
    Documentação de função atualizada.

--- util/email.php
+++ util/email.php
@@ -2,6 +2,16 @@
 require_once __DIR__ . "/../definicoes.php";
 require_once __DIR__ . "/PHPMailer/PHPMailerAutoload.php";
 
+
+/**
+ * Realiza o envio de uma mensagem eletrônica
+ * a uma lista de endereços de email.
+ * @param  string $assunto       Assunto da mensagem
+ * @param  string $corpo         Corpo da mensagem
+ * @param  mixed $destinatarios	 E-mails que receberão a mensagem, pode conter uma
+ *                               String com um e-mail ou um array com uma lista.
+ * @return mixed                 0 para sucesso ou a mensagem de falha em caso de erro.
+ */
 function enviarEmail($assunto, $corpo, $destinatarios) {
 	global $def_passes;
 
