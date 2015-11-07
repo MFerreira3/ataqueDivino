a039eede0221a4c052e1374071901cf3add5c9da
Matheus Reis <goodnews.room@hotmail.com>
2015-11-07 01:06:29 -0200

    Usuario.class.php
    Adição de método
    
    usuarioExistente
    Método criado para verificar nomes de usuário e endereços de email
    já cadastrados no sistema.

--- classes/Usuario.class.php
+++ classes/Usuario.class.php
@@ -4,5 +4,33 @@ class Usuario extends Record {
 	const TABLE = 'Usuarios'; // Nome da tabela no banco de dados.
 	const PK = 'codUsuario'; // Primary key da tabela.
 
+	static function usuarioExistente($usuario = null, $email = null) {
+		$usuarioExistente = false;
+
+		if (!empty($usuario)) {
+			$clausulas = array(
+						'select' => 'codUsuario',
+						'limit' =>  1
+					);
+
+			// Verifica se o usuário existe.
+			$where = array("usuario = ?", $usuario);
+			$usuarioExistente = Usuario::find($where, $clausulas);
+		}
+
+		if (!empty($email)) {
+			$clausulas = array(
+						'select' => 'codUsuario',
+						'limit' =>  1
+					);
+
+			// Verifica se o email já foi utilizado.
+			$where = array("email = ?", $email);
+			$usuarioExistente = Usuario::find($where, $clausulas);
+		}
+
+		return $usuarioExistente;
+	}
+
 }
 ?>
