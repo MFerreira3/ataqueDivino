55d95157fd85cf3a340df5c78c0390d6125229a9
Matheus Reis <goodnews.room@hotmail.com>
2015-11-10 21:17:12 -0200

    restricaoAjax.php
    Removida exibição de conteúdo HTML.

--- restricaoAjax.php
+++ restricaoAjax.php
@@ -1,13 +1,17 @@
 <?php
+/**
+ * Importar esse arquivo em uma página fará com que o acesso à mesma seja limitado
+ * ao uso de Ajax.
+ */
+
 $def_imprimirHTML = true;
 
 require_once __DIR__ . "/definicoes.php"; // Garante que os códigos de formatação tenham sido importados.
-require_once __DIR__ . "/debug.php"; // Garante que a função exibirErro esteja disponível para uso.
 
 // Verifica se o request vem de um algoritmo Ajax e depois se o mesmo vem do próprio host.
 $ajax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
 $host = isset($_SERVER['HTTP_REFERER']) ? strpos($_SERVER['HTTP_REFERER'], getenv('HTTP_HOST')) : "";
 if(!$ajax || $host === false) {
-	exibirErro('Acesso restrito', true);
+	die('Acesso restrito');
 }
 ?>
