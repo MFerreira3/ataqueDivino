85ab482f083b5d670ad262bf7d7d0a8364d8d57e
Matheus Reis <goodnews.room@hotmail.com>
2015-11-10 21:15:19 -0200

    Usuario.class.php
    Documentação atualizada.

--- classes/Usuario.class.php
+++ classes/Usuario.class.php
@@ -4,6 +4,14 @@ class Usuario extends Record {
 	const TABLE = 'Usuarios'; // Nome da tabela no banco de dados.
 	const PK = 'codUsuario'; // Primary key da tabela.
 
+	/**
+	 * Verifica se uma conta específica já foi
+	 * registrada a partir de seu nome de usuário ou e-mail.
+	 * @param  string $usuario Nome de usuário da conta.
+	 * @param  string $email   Endereço de e-mail da conta.
+	 * @return mixed           Conterá um array como resultado ou false
+	 *                         em caso de pesquisa sem retorno.
+	 */
 	static function usuarioExistente($usuario = null, $email = null) {
 		$usuarioExistente = false;
 
@@ -13,7 +21,7 @@ class Usuario extends Record {
 						'limit' =>  1
 					);
 
-			// Verifica se o usuário existe.
+			// Pesquisa pelo usuário especificado.
 			$where = array("usuario = ?", $usuario);
 			$usuarioExistente = Usuario::find($where, $clausulas);
 		}
