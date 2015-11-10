fb2d2c1ce3bf619a3b67c8a4b06b692a4c97849c
Matheus Reis <goodnews.room@hotmail.com>
2015-11-10 21:19:26 -0200

    calcularCost.php
    Script que calcula e define um valor padrão de cost
    para a geração de hashes, seu resultado é gravado no
    arquivo ps.json.

---
+++ calcularCost.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Calcula um cost ideal para o hardware utilizado como host e
+ * grava o valor encontrado no ps.json. Rode este código
+ * sempre que for necessário atualizar o cost.
+ */
+require_once "definicoes.php";
+
+// Define um cost mínimo.
+$cost = 8;
+// Realiza o teste com um cost cada vez maior até que o tempo de criação seja maior ou igual a 50 milissegundos.
+do {
+    $cost++;
+    $inicio = microtime(true);
+    password_hash("AllYourPassAreBelongToUs@123", PASSWORD_DEFAULT, ["cost" => $cost]);
+    $fim = microtime(true);
+} while (($fim - $inicio) < 0.05);
+
+// Registra o cost e armazena no ps.json.
+$def_passes->costPadrao = $cost;
+$diretorioPS = __DIR__ . '\ps.json';
+file_put_contents ($diretorioPS, json_encode($def_passes));
+
+?>
